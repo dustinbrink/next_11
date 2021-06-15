b342ba6a5045891843e1c8abbfc6694b870e02af
dustinbrink <dustinbrink@users.noreply.github.com>
2021-06-15 13:30:54 -0600

    add assets

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import "react-multi-carousel/lib/styles.css";
+import "react-modal-video/css/modal-video.min.css";
+import "rc-drawer/assets/index.css";
+import "typeface-dm-sans";
+import "styles/globals.css";
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+export default MyApp;
